43553d210208f6c84bbfb092e99b86560dd74aeb
AngeloContrera5 <angelocontreras628@gmail.com>
2022-07-27 17:20:18 -0500

    agregando la bd a heroku

--- src/database.js
+++ src/database.js
@@ -1,9 +1,15 @@
 const mysql = require("mysql2");
+
+DB_HOST = process.env.DB_HOST;
+DB_USER = process.env.DB_USER;
+DB_PASSWORD = process.env.DB_PASSWORD;
+DB_NAME = process.DB_NAME;
+
 const mysqlConnection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "mysql",
-  database: "your_daily_words_db",
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
 });
 
 mysqlConnection.connect(function (err) {
